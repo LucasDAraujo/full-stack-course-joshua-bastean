e6c1e9b5affe0c93e48ceb3f77df1f02c2b0f64c
lucasDAraujo <lukashalan@hotmail.com>
2021-11-02 16:28:57 -0500

    Modify route created

--- yelp_games/app.js
+++ yelp_games/app.js
@@ -3,12 +3,12 @@ const config = require("./config");
 
 /* ------------------------------- NPM IMPORTS ------------------------------ */
 const mongoose = require("mongoose");
-
 //Connect to DB
 mongoose.connect(config.database.connection());
 const express = require("express");
 const app = express();
 const port = 3000;
+const methodOverride = require("method-override");
 
 /* ------------------------------ ROUTE IMPORTS ----------------------------- */
 const gameRoutes = require("./routes/games");
@@ -19,24 +19,24 @@ const mainRoutes = require("./routes/main");
 const Game = require("./models/game");
 const Comment = require("./models/comment");
 
-/* ----------------------- OTHER CONFIGURATIONS OF APP ---------------------- */
-// 1 - Set ejs
-// 2- Make the public folder visible
-// 3 - Set urlEnconded from express true
-app.set("view engine", "ejs");
-app.use(express.static("public"));
+/* ----------------------- OTHER CONFIGURATIONS ---------------------- */
+
+app.set("view engine", "ejs"); // 1 - Set ejs
+app.use(express.static("public")); // 2- Make the public folder visible
 app.use(
+    // 3 - Set urlEnconded from express true
     express.urlencoded({
         extended: true,
     })
 );
+app.use(methodOverride("_method")); // 4 - Set method-override
 
 /* ------------------------------- USING ROUTES ------------------------------- */
-app.use("/games",gameRoutes);
-app.use("/games/:id/comments",commentRoutes);
+app.use("/games", gameRoutes);
+app.use("/games/:id/comments", commentRoutes);
 
 //MAIN ROUTES(WITH 404 ROUTE!)
-app.use("/",mainRoutes);
+app.use("/", mainRoutes);
 
 /* --------------------------------- LISTEN --------------------------------- */
 app.listen(port, () => console.log(`Yelp_games is working on port ${port}`));
--- yelp_games/routes/games.js
+++ yelp_games/routes/games.js
@@ -2,12 +2,20 @@ const express = require("express");
 const router = express.Router();
 const Game = require("../models/game");
 const Comment = require("../models/comment");
+
+const fixForm = (reqBody) => {
+    reqBody.genre = reqBody.genre.toLowerCase().replace(" ", "");
+    return reqBody;
+};
+
 //games GET route
 router.get("/", (req, res) => {
     Game.find()
         .exec()
         .then((foundGames) => {
-            res.render("games", { games: foundGames });
+            res.render("games", {
+                games: foundGames,
+            });
         })
         .catch((err) => {
             console.log(err);
@@ -17,13 +25,8 @@ router.get("/", (req, res) => {
 
 //games form POST route
 router.post("/", (req, res) => {
-    //Using spread operator to catch the form values
+    const newGame = fixForm(req.body);
 
-    const newGame = {
-        ...req.body,
-    };
-    newGame.genre = newGame.genre.toLowerCase().replace(" ", "");
-    console.log(newGame.genre);
     Game.create(newGame)
         .then((game) => {
             res.redirect("/games");
@@ -41,28 +44,55 @@ router.get("/new", (req, res) => {
 
 //Game show route
 router.get("/:id", (req, res) => {
-    Game.findById(req.params.id)
+    const id = req.params.id;
+    Game.findById(id)
         .exec()
         //Search the comments related to the specific game ID and shows the page
         .then((game) => {
-            Comment.find({ gameId: req.params.id }, (err, comments) => {
-                //"game" comes from .then((game)), and comments is the callback argument
-                err
-                    ? res.send(err)
-                    : res.render("games_show", { game, comments });
-            });
+            Comment.find(
+                {
+                    gameId: id,
+                },
+                (err, comments) => {
+                    //"game" comes from .then((game)), and comments is the callback argument
+                    err
+                        ? res.send(err)
+                        : res.render("games_show", {
+                              game,
+                              comments,
+                          });
+                }
+            );
         })
         .catch((err) => res.send(err));
 });
 
 router.get("/:id/edit", (req, res) => {
+    const id = req.params.id;
+
     //Get the game from the database
-    Game.findById(req.params.id)
+    Game.findById(id)
         .exec()
         .then((game) => {
             //Render de the edit form, passing in that game
-            res.render("games_edit", { game });
+            res.render("games_edit", {
+                game,
+            });
         });
+});
+
+router.put("/:id", (req, res) => {
+    const id = req.params.id;
+    const game = fixForm(req.body);
+    Game.findByIdAndUpdate(id, game, {
+        new: true,
+    })
+        .exec()
+        .then((updatedComic) => {
+            console.log(updatedComic);
+            res.redirect(`/games/${id}`);
+        })
+        .catch((err) => res.send(`ERROR:${err}`));
 });
 
 module.exports = router;
