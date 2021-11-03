a74ac2720109bf6e33291ae234395e422e03d62a
lucasDAraujo <lukashalan@hotmail.com>
2021-11-02 20:58:17 -0500

    Changed my code organization

--- yelp_games/app.js
+++ yelp_games/app.js
@@ -1,41 +1,61 @@
-/* ------------------------------ CONFIG IMPORT ----------------------------- */
+/* -------------------------------------------------------------------------- */
+/*                                   IMPORTS                                  */
+/* -------------------------------------------------------------------------- */
+
+//- CONFIG IMPORT -
 const config = require("./config");
 
-/* ------------------------------- NPM IMPORTS ------------------------------ */
+//- NPM IMPORTS -
 const mongoose = require("mongoose");
-//Connect to DB
-mongoose.connect(config.database.connection());
 const express = require("express");
 const app = express();
 const port = 3000;
 const methodOverride = require("method-override");
+const morgan = require("morgan");
 
-/* ------------------------------ ROUTE IMPORTS ----------------------------- */
+//- ROUTE IMPORTS -
 const gameRoutes = require("./routes/games");
 const commentRoutes = require("./routes/comments");
 const mainRoutes = require("./routes/main");
 
-/* ----------------------------- MODEL IMPORTS ----------------------------- */
+//- MODEL IMPORTS -
 const Game = require("./models/game");
 const Comment = require("./models/comment");
 
-/* ----------------------- OTHER CONFIGURATIONS ---------------------- */ 
-app.set("view engine", "ejs"); // 1 - Set ejs
-app.use(express.static("public")); // 2- Make the public folder visible
+/* -------------------------------------------------------------------------- */
+/*                                 DEVELOPMENT                                */
+/* -------------------------------------------------------------------------- */
+//Morgan
+app.use(morgan("tiny"));
+
+//Seed the DB
+
+/* -------------------------------------------------------------------------- */
+/*                               CONFIGURATIONS                               */
+/* -------------------------------------------------------------------------- */
+//connect to DB
+mongoose.connect(config.database.connection());
+
+//Express config
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+//Url encode config
 app.use(
-    // 3 - Set urlEnconded from express true
     express.urlencoded({
         extended: true,
     })
 );
-app.use(methodOverride("_method")); // 4 - Set method-override
 
-/* ------------------------------- USING ROUTES ------------------------------- */
+//Method override config
+app.use(methodOverride("_method"));
+
+//Route config
 app.use("/games", gameRoutes);
 app.use("/games/:id/comments", commentRoutes);
-
-//MAIN ROUTES(WITH 404 ROUTE!)
 app.use("/", mainRoutes);
 
-/* --------------------------------- LISTEN --------------------------------- */
+/* -------------------------------------------------------------------------- */
+/*                                   LISTEN                                   */
+/* -------------------------------------------------------------------------- */
 app.listen(port, () => console.log(`Yelp_games is working on port ${port}`));
