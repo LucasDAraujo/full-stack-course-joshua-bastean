e077cca04418197555c9791709750243316d060a
lucasDAraujo <lukashalan@hotmail.com>
2021-11-10 11:19:13 -0500

    Changed identation

--- yelp_games/models/game.js
+++ yelp_games/models/game.js
@@ -3,21 +3,21 @@ const mongoose = require("mongoose");
 
 //Using mongoose.schema to create the ganeSchema
 const gameSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    image: String,
-    developer: String,
-    publisher: String,
-    genre: String,
-    date: Date,
+  title: String,
+  description: String,
+  image: String,
+  developer: String,
+  publisher: String,
+  genre: String,
+  date: Date,
 });
 
 //Compiling the game schema into a more advanced constant with CAPITAL C using  mongoose.model
 //and exporting
 
 gameSchema.index({
-  '$**':'text'
-})
+  "$**": "text",
+});
 //On all fields, index any text
 
 module.exports = mongoose.model("Game", gameSchema);
