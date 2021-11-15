bb7f7db402e33d1f2edce74dbba7f5daafe6b66e
lucasDAraujo <lukashalan@hotmail.com>
2021-11-15 11:37:02 -0500

    Added account page

--- yelp_games/routes/main.js
+++ yelp_games/routes/main.js
@@ -4,6 +4,18 @@ const router = express.Router();
 // root route
 router.get("/", (req, res) => res.render("landing"));
 
+router.get("/account", isLoggedIn, (req, res) => {
+    res.render("account");
+});
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    } else {
+        res.redirect("/login");
+    }
+}
+
 router.get("*", (req, res) => res.send("That's a 404"));
 
 module.exports = router;
